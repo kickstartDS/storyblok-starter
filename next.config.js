fb7101a3e882796476008840070ef2ccee26c453
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-01-21 12:39:29 +0100

    fix: update Next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'https://app.storyblok.com';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: [
@@ -9,4 +23,19 @@ const nextConfig = {
   ],
 };
 
-module.exports = nextConfig;
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+  ...nextConfig,
+};

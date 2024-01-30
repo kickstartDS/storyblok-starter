bab52926fd338c6e1dee8b1dea41dda6465e8cbf
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-01-30 12:40:02 +0100

    fix: ESLint warnings

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -17,22 +17,25 @@ import { PictureProps } from "@kickstartds/base/lib/picture/typing";
 const Link = forwardRef<
   HTMLAnchorElement,
   AnchorHTMLAttributes<HTMLAnchorElement>
->(({ href, ...props }, ref) => {
-  const linkTarget =
-    (
-      href as unknown as {
-        cached_url: string;
-        story?: { full_slug: string; url: string; slug: string };
-      }
-    ).cached_url ||
-    (
-      href as unknown as {
-        cached_url: string;
-        story?: { full_slug: string; url: string; slug: string };
-      }
-    ).story?.full_slug;
-  return <LinkContextDefault ref={ref} {...props} href={linkTarget} />;
-});
+>(
+  // eslint-disable-next-line react/display-name
+  ({ href, ...props }, ref) => {
+    const linkTarget =
+      (
+        href as unknown as {
+          cached_url: string;
+          story?: { full_slug: string; url: string; slug: string };
+        }
+      ).cached_url ||
+      (
+        href as unknown as {
+          cached_url: string;
+          story?: { full_slug: string; url: string; slug: string };
+        }
+      ).story?.full_slug;
+    return <LinkContextDefault ref={ref} {...props} href={linkTarget} />;
+  }
+);
 
 const LinkProvider: FC<PropsWithChildren> = (props) => (
   <LinkContext.Provider value={Link} {...props} />
@@ -42,10 +45,13 @@ const LinkProvider: FC<PropsWithChildren> = (props) => (
 const Picture = forwardRef<
   HTMLImageElement,
   PictureProps & ImgHTMLAttributes<HTMLImageElement>
->(({ src, ...props }: PictureProps) => {
-  const filename = (src as unknown as { filename: string })?.filename;
-  return <PictureContextDefault {...props} src={filename} />;
-});
+>(
+  // eslint-disable-next-line react/display-name
+  ({ src, ...props }: PictureProps) => {
+    const filename = (src as unknown as { filename: string })?.filename;
+    return <PictureContextDefault {...props} src={filename} />;
+  }
+);
 
 const PictureProvider: FC<PropsWithChildren> = (props) => (
   <PictureContext.Provider {...props} value={Picture} />

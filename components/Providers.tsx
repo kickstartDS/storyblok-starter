e3a5736e8c4c6e2915b3a4350902fe0565268798
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-01-30 14:41:40 +0100

    fix: improve Providers a bit

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -11,28 +11,54 @@ import {
   PictureContext,
 } from "@kickstartds/base/lib/picture";
 
-import { LinkContextDefault, LinkContext } from "@kickstartds/base/lib/link";
+import {
+  LinkContextDefault,
+  LinkContext,
+  LinkProps,
+} from "@kickstartds/base/lib/link";
 import { PictureProps } from "@kickstartds/base/lib/picture/typing";
 
 // TODO look for a better type for `href` in Storyblok
+type StoryblokLink = {
+  cached_url: string;
+  linktype: string;
+  story?: { full_slug: string; url: string; slug: string };
+  target?: string;
+  email?: string;
+};
+
+type StoryblokAsset = {
+  filename: string;
+};
+
+function isStoryblokLink(object: unknown): object is StoryblokLink {
+  return (object as StoryblokLink)?.linktype !== undefined;
+}
+
+function isStoryblokAsset(object: unknown): object is StoryblokAsset {
+  return (object as StoryblokAsset)?.filename !== undefined;
+}
+
 const Link = forwardRef<
   HTMLAnchorElement,
-  AnchorHTMLAttributes<HTMLAnchorElement>
+  LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>
 >(({ href, ...props }, ref) => {
-  const linkTarget =
-    (
-      href as unknown as {
-        cached_url: string;
-        story?: { full_slug: string; url: string; slug: string };
-      }
-    )?.cached_url ||
-    (
-      href as unknown as {
-        cached_url: string;
-        story?: { full_slug: string; url: string; slug: string };
-      }
-    )?.story?.full_slug;
-  return <LinkContextDefault ref={ref} {...props} href={linkTarget} />;
+  if (isStoryblokLink(href)) {
+    const linkTarget =
+      href.linktype === "email"
+        ? `mailto:${href.email}`
+        : href.cached_url || href.story?.full_slug;
+    return (
+      <LinkContextDefault
+        {...props}
+        ref={ref}
+        href={linkTarget}
+        target={href.target}
+      />
+    );
+  }
+
+  return <LinkContextDefault {...props} href={href} />;
 });
 
 const LinkProvider: FC<PropsWithChildren> = (props) => (
@@ -44,8 +70,11 @@ const Picture = forwardRef<
   HTMLImageElement,
   PictureProps & ImgHTMLAttributes<HTMLImageElement>
 >(({ src, ...props }: PictureProps) => {
-  const filename = (src as unknown as { filename: string })?.filename;
-  return <PictureContextDefault {...props} src={filename} />;
+  if (isStoryblokAsset(src)) {
+    const filename = (src as unknown as StoryblokAsset)?.filename;
+    return <PictureContextDefault {...props} src={filename} />;
+  }
+  return <PictureContextDefault {...props} src={src} />;
 });
 
 const PictureProvider: FC<PropsWithChildren> = (props) => (

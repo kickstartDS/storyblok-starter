a56a561d99bfd03a488b8480ffca37e8b5b26ed3
Lukas Mestel <lukas.mestel@ruhmesmeile.com>
2024-02-01 10:17:51 +0100

    fix: render tag labels in blog teaser

--- components/BlogOverview.tsx
+++ components/BlogOverview.tsx
@@ -1,4 +1,8 @@
 import { storyblokEditable } from "@storyblok/react";
+import {
+  TagLabelContext,
+  TagLabelContextDefault,
+} from "@kickstartds/base/lib/tag-label";
 import { BlogOverview as BlogOverviewComponent } from "@kickstartds/ds-agency/blog-overview";
 import { BlogOverviewStoryblok } from "@/types/components-schema";
 
@@ -6,19 +10,26 @@ type PageProps = {
   blok: BlogOverviewStoryblok;
 };
 
+const Tag = ({ label, ...props }: any) => (
+  <TagLabelContextDefault label={label?.entry} {...props} />
+);
+
 const BlogOverview: React.FC<PageProps> = ({ blok }) => {
   if (blok) {
     const { seo, latest, more } = blok;
     return (
       <main {...storyblokEditable(blok)}>
-        <BlogOverviewComponent
-          // @ts-expect-error
-          latest={latest?.[0]}
-          // @ts-expect-error
-          seo={seo?.[0]}
-          // @ts-expect-error
-          more={more}
-        />
+        {/* @ts-expect-error */}
+        <TagLabelContext.Provider value={Tag}>
+          <BlogOverviewComponent
+            // @ts-expect-error
+            latest={latest?.[0]}
+            // @ts-expect-error
+            seo={seo?.[0]}
+            // @ts-expect-error
+            more={more}
+          />
+        </TagLabelContext.Provider>
       </main>
     );
   }

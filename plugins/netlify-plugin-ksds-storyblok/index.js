df7f8c89e1992068080325c38aca67a553520057
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-03-03 12:26:28 +0100

    fix: move build plugin to CJS

--- plugins/netlify-plugin-ksds-storyblok/index.js
+++ plugins/netlify-plugin-ksds-storyblok/index.js
@@ -1,7 +1,8 @@
-export const onPreBuild = async ({ utils }) => {
-  await utils.cache.restore("./cms/presets.123456.json");
-};
-
-export const onPostBuild = async ({ utils }) => {
-  await utils.cache.save("./cms/presets.123456.json");
+module.exports = {
+  onPreBuild: async ({ utils }) => {
+    await utils.cache.restore("./cms/presets.123456.json");
+  },
+  onPostBuild: async ({ utils }) => {
+    await utils.cache.save("./cms/presets.123456.json");
+  },
 };

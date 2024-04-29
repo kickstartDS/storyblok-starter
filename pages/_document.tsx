b1fff3d4addfe0de6f49a1df72dfed5482220363
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-04-29 12:03:25 +0200

    fix: add lang attribute

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,7 +28,7 @@ class KsDocument extends Document<any> {
     const { pageProps } = this.props;
 
     return (
-      <Html className={pageProps.fontClassNames}>
+      <Html className={pageProps.fontClassNames} lang="en">
         <Head />
         <body>
           <Main />

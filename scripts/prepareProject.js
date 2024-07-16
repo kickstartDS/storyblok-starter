31f559108144db38567695e3aa6b0b18b1eeba4c
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2024-07-16 17:51:15 +0200

    fix: add video capabilities to project setup script

--- scripts/prepareProject.js
+++ scripts/prepareProject.js
@@ -9,6 +9,8 @@ const jsonpointer = require("jsonpointer");
 const designSystemPresets = require("@kickstartds/ds-agency/presets.json");
 const generatedComponents = require("../cms/components.123456.json");
 const initialStory = require("../resources/story.json");
+const ffprobe = require("ffprobe");
+const ffprobeStatic = require("ffprobe-static");
 
 require("dotenv").config({ path: ".env.local" });
 
@@ -40,7 +42,7 @@ const groupToComponentName = (name) => name.split("/").pop().trim();
 const upload = (signed_request, file) => {
   return new Promise((resolve, reject) => {
     const form = new FormData();
-    for (let key in signed_request.fields) {
+    for (const key in signed_request.fields) {
       form.append(key, signed_request.fields[key]);
     }
     form.append("file", fs.createReadStream(file));
@@ -53,15 +55,21 @@ const upload = (signed_request, file) => {
 
 const signedUpload = async (fileName, assetFolderId) => {
   return new Promise(async (resolve) => {
-    let dimensions = sizeOf(
-      "./node_modules/@kickstartds/ds-agency/dist/static/" + fileName
-    );
+    const fullPath = `./node_modules/@kickstartds/ds-agency-premium/dist/static/${fileName}`;
+    let size = "";
+    if (fileName.includes("mp4")) {
+      const probe = await ffprobe(fullPath, { path: ffprobeStatic.path });
+      size = `${probe.streams[0].width}x${probe.streams[0].height}`;
+    } else {
+      const dimensions = sizeOf(fullPath);
+      size = `${dimensions.width}x${dimensions.height}`;
+    }
 
     const assetResponse = await Storyblok.post(
       `spaces/${process.env.NEXT_STORYBLOK_SPACE_ID}/assets/`,
       {
         filename: fileName,
-        size: `${dimensions.width}x${dimensions.height}`,
+        size,
         asset_folder_id: assetFolderId || null,
       }
     );
